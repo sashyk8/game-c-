c2e429429ea710f503ecde94e6e3929199e0560c
sashyk8 <aligsafina@edu.hse.ru>
2024-06-02 02:30:05 +0300

    'soonrenderimtired02junesashuk'

--- win.cpp
+++ win.cpp
@@ -1,20 +1,98 @@
-//заголовочный файл, в котором объявляются функции, предоставляющие интерфейс доступа к виндовс апи
-#include <windows.h> //чтобы мы могли использовать функционал в нашей программе, предоставляемый ос 
+//задебажить и поправить много всего да 
+#include <windows.h> 
+
+bool running = true;
+
+struct Render_State {
+    int height, width;
+    void* memory;
+
+    BITMAPINFO bitmapinfo;
+};
+
+Render_State render_state;
+//что дальше то убрать потом сделать замену везде с buffer на render_state.
+void* buffer_memory;
+int buffer_width;
+int buffer_height;
+BITMAPINFO buffer_bitmap_info;
+
 
 LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	LRESULT result = 0;
+
+	switch (uMsg) {
+		case WM_CLOSE:
+		case WM_DESTROY: {
+			running = false;
+		} break;
+
+        case WM_SIZE: {
+			RECT rect;
+			GetClientRect(hwnd, &rect);
+            buffer_width = rect.right - rect.left;
+            buffer_height = rect.bottom - rect.top;
+
+            int buffer_size = buffer_width * buffer_height * sizeof(unsigned int);
+
+            if (buffer_memory) VirtualFree(buffer_memory, 0, MEM_RELEASE);
+            buffer_memory = VirtualAlloc(0, buffer_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+            buffer_bitmap_info.bmiHeader.biSize = sizeof(buffer_bitmap_info.bmiHeader);
+            buffer_bitmap_info.bmiHeader.biWidth = buffer_width;
+            buffer_bitmap_info.bmiHeader.biHeight = buffer_height;
+            buffer_bitmap_info.bmiHeader.biPlanes = 1;
+            buffer_bitmap_info.bmiHeader.biBitCount = 32;
+            buffer_bitmap_info.bmiHeader.biCompression = BI_RGB;
+
+
+
+
+			
+		
+		} break;
+
+		default: {
+			result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}
+	}
+	return result;
 }
 
-//точка входа в графическое приложение виндовс, предоставляемая пользователем, винмэйн - условное имя для точки входа 
 int WinMain(HINSTANCE hInstanse, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
     //создаем класс виндовс
     WNDCLASS window_class = {};
     window_class.style = CS_HREDRAW | CS_VREDRAW;
     window_class.lpszClassName = "game window class";
     window_class.lpfnWndProc = window_callback;
-    //...
+
+    //RegisterClas
     RegisterClass(&window_class);
 
     //...
-    CreateWindow(window_class.lpszClassName, "game", WS_OVERLAPPEDWINDOW | WS_VISIBLE, )
+    HWND window = CreateWindow(window_class.lpszClassName, "game", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstanse, 0);
+    HDC hdc = GetDC(window);
 }
+
+    while (running) {
+		// Input
+		MSG message;
+
+		while (PeekMessage(&message, window, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&message);
+            DispatchMessage(&message);
+        }
+
+        //simulate
+        unsigned int* pixel = (unsigned int*)buffer_memory;
+        for (int y = 0; y < buffer_height; y++) {
+            for (int x = 0; x < buffer_width; x++) {
+                *pixel++ = x*y; //тут можно разное еще поработать плюс рендер
+            }
+        }
+
+
+        //render (с гейдаром)
+        StretchDIBits(hdc, 0, 0, buffer_width, buffer_height, 0, 0, buffer_width, buffer_height, buffer_memory, &buffer_bitmap_info, DIB_RGB_COLORS, SRCCOPY);
+
+    }
