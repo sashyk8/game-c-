56ba395a15e99bca46bbae74280da81e98435723
geekiskatel <65942953+geekiskatel@users.noreply.github.com>
2024-06-19 19:46:33 +0300

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,11 +1,8 @@
-/**
- * @file game_simulation.cpp
- * @brief Simple Pong-like game simulation.
- */
-
 #define is_down(b) input->buttons[b].is_down
 #define pressed(b) (input->buttons[b].is_down && input->buttons[b].changed)
 #define released(b) (!input->buttons[b].is_down && input->buttons[b].changed)
+
+#include "game.h" 
 
 float player_1_p, player_1_dp, player_2_p, player_2_dp;
 float arena_half_size_x = 85, arena_half_size_y = 45;
@@ -14,15 +11,8 @@ float ball_p_x, ball_p_y, ball_dp_x = 130, ball_dp_y, ball_half_size = 1;
 
 int player_1_score, player_2_score;
 
-/**
- * @brief Simulates the player's movement.
- * 
- * @param p Pointer to the player's position.
- * @param dp Pointer to the player's velocity.
- * @param ddp Player's acceleration.
- * @param dt Delta time for the simulation step.
- */
-void SimulatePlayer(float *p, float *dp, float ddp, float dt) {
+
+void simulate_player(float *p, float *dp, float ddp, float dt) {
   ddp -= *dp * 10.f;
 
   *p = *p + *dp * dt + ddp * dt * dt * .5f;
@@ -31,62 +21,45 @@ void SimulatePlayer(float *p, float *dp, float ddp, float dt) {
   if (*p + player_half_size_y > arena_half_size_y) {
     *p = arena_half_size_y - player_half_size_y;
     *dp = 0;
-  } else if (*p - player_half_size_y < -arena_half_size_y) {
+  }
+  else if (*p - player_half_size_y < -arena_half_size_y) {
     *p = -arena_half_size_y + player_half_size_y;
     *dp = 0;
   }
 }
 
-/**
- * @brief Checks for axis-aligned bounding box collision.
- * 
- * @param p1x X position of the first box.
- * @param p1y Y position of the first box.
- * @param hs1x Half size in X direction of the first box.
- * @param hs1y Half size in Y direction of the first box.
- * @param p2x X position of the second box.
- * @param p2y Y position of the second box.
- * @param hs2x Half size in X direction of the second box.
- * @param hs2y Half size in Y direction of the second box.
- * @return true if the boxes collide.
- * @return false if the boxes do not collide.
- */
-bool AabbVsAabb(float p1x, float p1y, float hs1x, float hs1y,
-                float p2x, float p2y, float hs2x, float hs2y) {
+
+bool aabb_vs_aabb(float p1x, float p1y, float hs1x, float hs1y,
+  float p2x, float p2y, float hs2x, float hs2y) {
   return (p1x + hs1x > p2x - hs2x &&
-          p1x - hs1x < p2x + hs2x &&
-          p1y + hs1y > p2y - hs2y &&
-          p1y + hs1y < p2y + hs2y);
+    p1x - hs1x < p2x + hs2x &&
+    p1y + hs1y > p2y - hs2y &&
+    p1y + hs1y < p2y + hs2y);
 }
 
-/**
- * @brief Enumeration of game modes.
- */
 enum Gamemode {
-  kMenu, /**< Menu mode */
-  kGameplay, /**< Gameplay mode */
+  GM_MENU,
+  GM_GAMEPLAY,
 };
 
-Gamemode current_gamemode; /**< Current game mode */
-int hot_button; /**< Current selected button in menu */
-bool enemy_is_ai; /**< Is the enemy controlled by AI */
+Gamemode current_gamemode;
+int hot_button;
+bool enemy_is_ai;
 
-/**
- * @brief Simulates the game state.
- * 
- * @param input Pointer to the input structure.
- * @param dt Delta time for the simulation step.
- */
-void SimulateGame(Input* input, float dt) {
-  DrawRect(0, 0, arena_half_size_x, arena_half_size_y, 0xffaa33);
-  DrawArenaBorders(arena_half_size_x, arena_half_size_y, 0xff5500);
 
-  if (current_gamemode == kGameplay) {
+void simulate_game(Input* input, float dt) {
+  draw_rect(0, 0, arena_half_size_x, arena_half_size_y, 0xffaa33);
+  draw_arena_borders(arena_half_size_x, arena_half_size_y, 0xff5500);
+
+
+  if (current_gamemode == GM_GAMEPLAY) {
     float player_1_ddp = 0.f;
     if (!enemy_is_ai) {
       if (is_down(BUTTON_UP)) player_1_ddp += 2000;
       if (is_down(BUTTON_DOWN)) player_1_ddp -= 2000;
     } else {
+      //if (ball_p_y > player_1_p+2.f) player_1_ddp += 1300;
+      //if (ball_p_y < player_1_p-2.f) player_1_ddp -= 1300;
       player_1_ddp = (ball_p_y - player_1_p) * 100;
       if (player_1_ddp > 1300) player_1_ddp = 1300;
       if (player_1_ddp < -1300) player_1_ddp = -1300;
@@ -96,19 +69,20 @@ void SimulateGame(Input* input, float dt) {
     if (is_down(BUTTON_W)) player_2_ddp += 2000;
     if (is_down(BUTTON_S)) player_2_ddp -= 2000;
 
-    SimulatePlayer(&player_1_p, &player_1_dp, player_1_ddp, dt);
-    SimulatePlayer(&player_2_p, &player_2_dp, player_2_ddp, dt);
+    simulate_player(&player_1_p, &player_1_dp, player_1_ddp, dt);
+    simulate_player(&player_2_p, &player_2_dp, player_2_ddp, dt);
+
 
     // Simulate Ball
     {
       ball_p_x += ball_dp_x * dt;
       ball_p_y += ball_dp_y * dt;
 
-      if (AabbVsAabb(ball_p_x, ball_p_y, ball_half_size, ball_half_size, 80, player_1_p, player_half_size_x, player_half_size_y)) {
+      if (aabb_vs_aabb(ball_p_x, ball_p_y, ball_half_size, ball_half_size, 80, player_1_p, player_half_size_x, player_half_size_y)) {
         ball_p_x = 80 - player_half_size_x - ball_half_size;
         ball_dp_x *= -1;
         ball_dp_y = (ball_p_y - player_1_p) * 2 + player_1_dp * .75f;
-      } else if (AabbVsAabb(ball_p_x, ball_p_y, ball_half_size, ball_half_size, -80, player_2_p, player_half_size_x, player_half_size_y)) {
+      } else if (aabb_vs_aabb(ball_p_x, ball_p_y, ball_half_size, ball_half_size, -80, player_2_p, player_half_size_x, player_half_size_y)) {
         ball_p_x = -80 + player_half_size_x + ball_half_size;
         ball_dp_x *= -1;
         ball_dp_y = (ball_p_y - player_2_p) * 2 + player_2_dp * .75f;
@@ -137,31 +111,34 @@ void SimulateGame(Input* input, float dt) {
       }
     }
 
-    DrawNumber(player_1_score, -10, 40, 1.f, 0xbbffbb);
-    DrawNumber(player_2_score, 10, 40, 1.f, 0xbbffbb);
+    draw_number(player_1_score, -10, 40, 1.f, 0xbbffbb);
+    draw_number(player_2_score, 10, 40, 1.f, 0xbbffbb);
 
     // Rendering
-    DrawRect(ball_p_x, ball_p_y, ball_half_size, ball_half_size, 0xffffff);
-    DrawRect(80, player_1_p, player_half_size_x, player_half_size_y, 0xff0000);
-    DrawRect(-80, player_2_p, player_half_size_x, player_half_size_y, 0xff0000);
+    draw_rect(ball_p_x, ball_p_y, ball_half_size, ball_half_size, 0xffffff);
+
+    draw_rect(80, player_1_p, player_half_size_x, player_half_size_y, 0xff0000);
+    draw_rect(-80, player_2_p, player_half_size_x, player_half_size_y, 0xff0000);
 
   } else {
+
     if (pressed(BUTTON_LEFT) || pressed(BUTTON_RIGHT)) {
       hot_button = !hot_button;
     }
 
-    if (pressed(BUTTON_ENTER)) {
-      current_gamemode = kGameplay;
+Александра, [19.06.2024 19:29]
+if (pressed(BUTTON_ENTER)) {
+      current_gamemode = GM_GAMEPLAY;
       enemy_is_ai = hot_button ? 0 : 1;
     }
 
     if (hot_button == 0) {
-      DrawText("SINGLE PLAYER", -80, -10, 1, 0xff0000);
-      DrawText("MULTIPLAYER", 20, -10, 1, 0xaaaaaa);
+      draw_text("SINGLE PLAYER", -80, -10, 1, 0xff0000);
+      draw_text("MULTIPLAYER", 20, -10, 1, 0xaaaaaa);
     } else {
-      DrawText("SINGLE PLAYER", -80, -10, 1, 0xaaaaaa);
-      DrawText("MULTIPLAYER", 20, -10, 1, 0xff0000);
+      draw_text("SINGLE PLAYER", -80, -10, 1, 0xaaaaaa);
+      draw_text("MULTIPLAYER", 20, -10, 1, 0xff0000);
     }
+    
   }
 }
-
