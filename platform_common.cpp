fef7a67abeb77d15ff2e4167a53becf5cdccab48
geekiskatel <65942953+geekiskatel@users.noreply.github.com>
2024-06-20 15:32:57 +0300

    Update platform_common.cpp
    Финальная версия

--- platform_common.cpp
+++ platform_common.cpp
@@ -1,20 +1,37 @@
+/**
+ * @file input.h
+ * @brief Содержит определения для структуры ввода и кнопок.
+ */
+
+/**
+ * @struct Button_State
+ * @brief Представляет состояние кнопки.
+ */
 struct Button_State {
-  bool is_down;
-  bool changed;
+    bool is_down; /**< Кнопка нажата. */
+    bool changed; /**< Состояние кнопки изменилось. */
 };
 
+/**
+ * @enum
+ * @brief Перечисление всех кнопок.
+ */
 enum {
-  BUTTON_UP,
-  BUTTON_DOWN,
-  BUTTON_W,
-  BUTTON_S,
-  BUTTON_LEFT,
-  BUTTON_RIGHT,
-  BUTTON_ENTER,
+    BUTTON_UP, /**< Кнопка "Вверх". */
+    BUTTON_DOWN, /**< Кнопка "Вниз". */
+    BUTTON_W, /**< Кнопка "W". */
+    BUTTON_S, /**< Кнопка "S". */
+    BUTTON_LEFT, /**< Кнопка "Влево". */
+    BUTTON_RIGHT, /**< Кнопка "Вправо". */
+    BUTTON_ENTER, /**< Кнопка "Enter". */
 
-  BUTTON_COUNT, 
+    BUTTON_COUNT, /**< Количество кнопок. */
 };
 
+/**
+ * @struct Input
+ * @brief Представляет состояние ввода.
+ */
 struct Input {
-  Button_State buttons[BUTTON_COUNT];
+    Button_State buttons[BUTTON_COUNT]; /**< Массив состояний кнопок. */
 };
